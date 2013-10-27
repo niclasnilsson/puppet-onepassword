82a125e39e412b2b3d309ab09bce248ba397eb20
Niclas Nilsson <niclas@niclasnilsson.se>
2013-10-27 21:13:15 +0100

    1Password 4.0.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,8 @@
 #
 #     include 1password
 class onepassword {
-  $host = 'd13itkw33a7sus.cloudfront.net'
-  $path = 'dist/1P/mac/1Password-3.8.21.zip'
+  $host = 'i.agilebits.com'
+  $path = 'dist/1P/mac4/1Password-4.0.2.zip'
 
   package { '1Password':
     source   => "https://${host}/${path}",
